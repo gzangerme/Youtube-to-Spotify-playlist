bf363c2da0e3c9a8d8fa65798e4e569711242d6c
Gabriel Zangerme <gzangelmii@gmail.com>
2020-03-27 18:47:01 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,8 @@
 import spotify_token as st
 
+#Make Sure to input the right information for it to generate the Spotify Token
 
-spotify_user_id = 'gzangerme'
-data = st.start_session("gzangerme","Saymyname799$")
+spotify_user_id = 'YOUR_USERNAME_ON_SPOTIFY'
+data = st.start_session('YOUR_USERNAME_ON_SPOTIFY','YOUR_PASSWORD_ON_SPOTIFY')
 spotify_token = data[0]
 expiration_date = data[1]
